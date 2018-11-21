48c87c3f68122263de61fcb0f8b381f372e75215
Lornatang <shiyipaisizuo@gmail.com>
2018-11-21 16:11:33 +0800

    Break up the code

---
+++ mnist/download.py
@@ -0,0 +1,22 @@
+import os
+
+import urllib.request
+import tensorflow as tf
+
+# Download dataset
+SOURCE_URL = "http://yann.lecun.com/exdb/mnist/"
+WORK_DIRECTORY = './data/mnist'
+
+
+def download(filename):
+    """Download the data from Yann's website, unless it"""
+    if not tf.gfile.Exists(WORK_DIRECTORY):
+        tf.gfile.MakeDirs(WORK_DIRECTORY)
+    filepath = os.path.join(WORK_DIRECTORY, filename)
+    if not tf.gfile.Exists(filepath):
+        filepath, _ = urllib.request.urlretrieve(
+            SOURCE_URL + filepath, filepath)
+        with tf.gfile.GFile(filepath) as f:
+            size = f.size()
+        print(f"Successfully downloaded {filename} {size} bytes")
+    return filepath
--- mnist/main.py
+++ mnist/main.py
@@ -4,13 +4,13 @@
 """
 
 import argparse
-import os
 import sys
-import gzip
 import time
 
+from tools import *
+from download import *
+
 import numpy as np
-import urllib.request
 import tensorflow as tf
 
 # Download dataset
@@ -37,55 +37,6 @@ def data_type():
         return tf.float16
     else:
         return tf.float32
-
-
-def download(filename):
-    """Download the data from Yann's website, unless it"""
-    if not tf.gfile.Exists(WORK_DIRECTORY):
-        tf.gfile.MakeDirs(WORK_DIRECTORY)
-    filepath = os.path.join(WORK_DIRECTORY, filename)
-    if not tf.gfile.Exists(filepath):
-        filepath, _ = urllib.request.urlretrieve(
-            SOURCE_URL + filepath, filepath)
-        with tf.gfile.GFile(filepath) as f:
-            size = f.size()
-        print(f"Successfully downloaded {filename} {size} bytes")
-    return filepath
-
-
-def extract_data(filename, num_images):
-    """Extract the images into a 4D tensor [image index, y, x, channels].
-
-    Values are rescaled from [0, 255] down to [-0.5, 0.5].
-    """
-    print(f"Extracting {filename}")
-    with gzip.open(filename) as bytestream:
-        bytestream.read(16)
-        buf = bytestream.read(
-            IMAGE_SIZE *
-            IMAGE_SIZE *
-            num_images *
-            NUM_CHANNELS)
-        data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-        data = (data - (PIXEL_DEPTH / 2.0)) / PIXEL_DEPTH
-        data = data.reshape(num_images, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS)
-        return data
-
-
-def extract_labels(filename, num_images):
-    """Extract the labels into a vector of int64 label IDs."""
-    print(f"Extracting {filename}")
-    with gzip.open(filename) as bytestream:
-        bytestream.read(8)
-        buf = bytestream.read(1 * num_images)
-        labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
-    return labels
-
-
-def error_rate(predictions, labels):
-    """Return the error rate base on dense predictions"""
-    return 100.00 - (100.00 * np.sum(np.argmax(predictions, 1) == labels) /
-                     predictions.shape[0])
 
 
 def main(_):
