97fe5ccc8322a4759c13b8afba8ca32254d4fbc0
Lornatang <shiyipaisizuo@gmail.com>
2018-11-22 08:28:55 +0800

    Create input_data.py

---
+++ caltech/input_data.py
@@ -0,0 +1,10 @@
+# input_data.py
+
+"""
+    We need to do sample and label classification of generated images of specified size,
+    obtain the get_files input by neural network,
+    and meanwhile input data for batch processing
+    in order to facilitate network training.
+"""
+
+
