3fe02bdbc95b50114ced0227ad730c4a5d8e5ca7
Lornatang <shiyipaisizuo@gmail.com>
2018-11-23 23:26:33 +0800

    Automatically categorize the generated files

--- caltech/caltech4.py
+++ caltech/caltech4.py
@@ -6,26 +6,31 @@ import tensorflow as tf
 import cv2
 import numpy as np
 
-INPUT_DIR = 'raw_data'
-OUTPUT_DIR = 'data'
+INPUT_DIR = 'raw_data/'
+OUTPUT_DIR = 'data/'
 
 LOGS_DIR = 'logs'
-
-# check output dir exists
-if not tf.gfile.Exists(OUTPUT_DIR):
-    tf.gfile.MakeDirs(OUTPUT_DIR)
-    
-# check logs dir exists
-if not tf.gfile.Exists(LOGS_DIR):
-    tf.gfile.MakeDirs(LOGS_DIR)
 
 # The type of recognition required
 CLASSES = {'airplane', 'car', 'face', 'motorbike'}
 
+# check output dir exists
+if not tf.gfile.Exists(OUTPUT_DIR):
+    tf.gfile.MakeDirs(OUTPUT_DIR)
 
-def sum_of_dir(dirpath):
+# check logs dir exists
+if not tf.gfile.Exists(LOGS_DIR):
+    tf.gfile.MakeDirs(LOGS_DIR)
+
+# create classes dir
+for _, dir_name in enumerate(CLASSES):
+    if not tf.gfile.Exists(OUTPUT_DIR + dir_name):
+        tf.gfile.MakeDirs(OUTPUT_DIR + dir_name)
+
+
+def sum_of_file(dirpath):
     """sum of images.
-    
+
     Args:
         dirpath: input dir
 
@@ -35,8 +40,8 @@ def sum_of_dir(dirpath):
     """
     num = 0
     for dir in os.listdir(dirpath):
-       for _ in os.listdir(dirpath + '/' + dir):
-           num += 1
+        for _ in os.listdir(dirpath + '/' + dir):
+            num += 1
     return num
 
 
@@ -44,26 +49,34 @@ def create_record():
     """make TFRecords raw_data"""
     writer = tf.python_io.TFRecordWriter("caltech_4.tfrecords")
     for index, name in enumerate(CLASSES):
+        if name == 'airplane':
+            index = 0
+        elif name == 'car':
+            index = 1
+        elif name == 'face':
+            index = 2
+        else:
+            index = 3
         class_path = INPUT_DIR + "/" + name + "/"
         for img_name in os.listdir(class_path):
-            img_path = class_path + img_name
+            img_path = os.path.join(class_path, img_name)
             img = cv2.imread(img_path)
             img = cv2.resize(
-                img, (64, 64), interpolation=cv2.INTER_NEAREST)  # resize img
+                img, (224, 224), interpolation=cv2.INTER_NEAREST)  # resize img
             data = img.tobytes()
-
             example = tf.train.Example(
                 features=tf.train.Features(feature={
-                    "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
-                    'raw_data': tf.train.Feature(bytes_list=tf.train.BytesList(value=[data]))
+                    'data': tf.train.Feature(bytes_list=tf.train.BytesList(value=[data])),
+                    'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[index]))
                 }))
+
             writer.write(example.SerializeToString())
     writer.close()
 
 
 def read_and_decode(filename):
     """Read the image into the tensorFlow queue.
-    
+
     Args:
         filename: read image to tf queue path.
 
@@ -81,27 +94,29 @@ def read_and_decode(filename):
     features = tf.parse_single_example(
         serialized_example,
         features={
-            'label': tf.FixedLenFeature([], tf.int64),
-            'raw_data': tf.FixedLenFeature([], tf.string)
+            'data': tf.FixedLenFeature([], tf.string),
+            'label': tf.FixedLenFeature([], tf.int64)
         })
+    data = features['data']
     label = features['label']
-    data = features['raw_data']
     data = tf.decode_raw(data, tf.uint8)
-    data = tf.reshape(data, [64, 64, 3])
-    # raw_data = tf.cast(raw_data, tf.float32) * (1. / 255) - 0.5
-    label = tf.cast(label, tf.int32)
+    data = tf.reshape(data, [224, 224, 3])
+    label = tf.cast(label, tf.int64)
     return data, label
 
 
 if __name__ == '__main__':
-    num_examples = sum_of_dir(INPUT_DIR)
-    print(f"Images total: {num_examples}.")
-    print(f"Start create record!")
+    num_examples = sum_of_file(INPUT_DIR)
+    print(f"Images total: {num_examples}.\n")
+    print(f"Start create record!\n")
     create_record()
+    print(f"Record create successful.")
     batch = read_and_decode('caltech_4.tfrecords')
+    # Distributed tensorflow
     init_op = tf.group(
-        tf.global_variables_initializer(),
-        tf.local_variables_initializer())
+        tf.initializers.local_variables(),
+        tf.initializers.global_variables()
+    )
 
     with tf.Session() as sess:
         # init all variables
@@ -109,19 +124,27 @@ if __name__ == '__main__':
         print("Init all variables complete!")
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
-
+        classes = None
         for i in range(num_examples):
             example, lab = sess.run(batch)
             # Convert the array to an image
             img = cv2.cvtColor(
                 np.asarray(example),
                 cv2.COLOR_RGB2BGR)
+            if lab == 0:
+                classes = 'airplane'
+            elif lab == 1:
+                classes = 'car'
+            elif lab == 2:
+                classes = 'face'
+            else:
+                classes = 'motorbike'
             cv2.imwrite(
                 OUTPUT_DIR +
                 '/' +
+                classes +
+                '/' +
                 str(i) +
-                'samples' +
-                str(lab) +
                 '.jpg',
                 img)
         print(f"Image written to '{OUTPUT_DIR}'.")
